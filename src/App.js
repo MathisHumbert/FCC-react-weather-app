227ce18e913a2d4cd41cae5ef521dac9545ceb89
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-01-19 15:13:51 +0100

    set up base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+let url = 'https://fcc-weather-api.glitch.me/api/current?';
 
+const initialState = {
+  name: '',
+  country: '',
+  temp: '',
+  temp_max: '',
+  temp_min: '',
+  description: '',
+  icon: '',
+};
 function App() {
+  const [error, setError] = useState(false);
+  const [weather, setWeather] = useState();
+  const getGeolocation = () => {
+    if (!navigator.geolocation) {
+      setError(true);
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      axios(`${url}lat=${lat}&lon=${lon}`)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => console.log(error));
+    });
+  };
+
+  useEffect(() => {
+    let ready = setInterval(() => {
+      if (document.readyState === 'complete') {
+        getGeolocation();
+        clearInterval(ready);
+      }
+    }, 100);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather App</h1>
     </div>
   );
 }
